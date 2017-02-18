f67b7b19388ec8c83fa1b92998345e9805a7ac0f
MrRobot9 <arjunhegde777@gmail.com>
2017-02-18 23:51:27 +0530

    updated version

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup,find_packages
 setup(
  name = 'android-tool', # write your own package name here                                                                                     
   packages = find_packages(),     
-  version = '1.1.1',
-  description = 'Script to ease few Android development',
+  version = '1.1.2',
+  description = 'Script which aids you in few Android development practises',
   author = 'Arjun Gurudatta Hegde',
   author_email = 'arjunhegde777@gmail.com',
   url = 'https://github.com/MrRobot9/android-tool', # use the URL to the github repo
